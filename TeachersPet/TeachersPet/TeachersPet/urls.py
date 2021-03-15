1e294dacb3f8241aca697aa61c437e9d3c942d64
michael.heinzinger <michael.heinzinger@gmail.com>
2021-03-14 20:39:06 -0500

    Created Student1.1 page and filled it with dummy data for now

--- TeachersPet/TeachersPet/TeachersPet/urls.py
+++ TeachersPet/TeachersPet/TeachersPet/urls.py
@@ -13,6 +13,7 @@ from users import views as users_views
 urlpatterns = [
     #path('home/', views.home, name='home'),
     path('', views.homepage, name='homepage'),
+    path('student1_1', views.student1_1, name='student1_1'),
     path('register/', users_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
--- TeachersPet/TeachersPet/app/views.py
+++ TeachersPet/TeachersPet/app/views.py
@@ -1,11 +1,26 @@
-"""
-Definition of views.
-"""
-
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
 
+dummy_data = [ 
+    {
+        'teacher': 'Prof John Doe',
+        'class': 'Class 1',
+        'content': 'A+',
+    },
+    {
+        'teacher': 'Prof Jane Doe',
+        'class': 'Class 2',
+        'content': 'A-',
+    }
+]
+
 def homepage(request):
     return render(request, 'app/homepage.html')
+
+def student1_1(request):
+    context = {
+        'dummy_data': dummy_data    # uses dummy data specified above for student1_1 view until we can replace with DB data
+    }
+    return render(request, 'app/student1_1.html', context)  # context argument allows dummy data above to be used
