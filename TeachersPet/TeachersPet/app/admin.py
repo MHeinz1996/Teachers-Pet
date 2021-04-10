61d0efd4d7a7d2d5cc7a9e9908452c3951d530b5
sally <sallygschmidt@gmail.com>
2021-04-09 21:52:07 -0400

    Added database tables to app\view.py, all tables available via admin

---
+++ TeachersPet/TeachersPet/app/admin.py
@@ -0,0 +1,29 @@
+#app/admin.py
+
+from django.contrib import admin
+
+from .models import LookupDepartment
+from .models import CourseAssignment
+from .models import CourseSchedule
+from .models import User
+from .models import CourseStudent
+from .models import LookupCourse
+from .models import LookupTerm
+from .models import StudentSubmission
+from .models import TeacherCertification
+from .models import DummyData
+from .models import DummyClass
+
+
+
+admin.site.register(LookupDepartment)
+admin.site.register(CourseAssignment)
+admin.site.register(CourseSchedule)
+admin.site.register(CourseStudent)
+admin.site.register(LookupCourse)
+admin.site.register(LookupTerm)
+admin.site.register(StudentSubmission)
+admin.site.register(TeacherCertification)
+admin.site.register(DummyData)
+admin.site.register(DummyClass)
+
--- TeachersPet/TeachersPet/app/views.py
+++ TeachersPet/TeachersPet/app/views.py
@@ -2,52 +2,67 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from datetime import datetime
 import datetime
+from .models import DummyClass
+from .models import DummyData
 
-dummy_data = [ 
-    {
-        'dept' : 'Math',
-        'teacher': 'Prof John Doe',
-        'class': 'Algebra 1',
-        'content': 'A+',
-    },
-    {
-        'dept' : 'English',
-        'teacher': 'Prof Jane Doe',
-        'class': 'English 101',
-        'content': 'A-',
-    }
-]
+
+ 
+
+
+username=User.username
+
+dummyclass=DummyClass.objects.all()
+dummy_data=DummyData.objects.all()
+#dummy_data=DummyData.objects.filter(student=username)
+
+
+
+#dummy_data = [ 
+#    {
+#        'dept' : 'Math',
+#        'teacher': 'Prof John Doe',
+#        'class': 'Algebra 1',
+#        'content': 'A+',
+#    },
+#    {
+#        'dept' : 'English',
+#        'teacher': 'Prof Jane Doe',
+#        'class': 'English 101',
+#        'content': 'A-',
+#    }
+#]
 
 newitem = 'hello'
 
-dummyclass = [
-    {
-        'datestart' : datetime.date(2021, 1, 1),
-        'dateend' : datetime.date(2021, 3, 4),
-        'firstname' : 'steve',
-        'lastname' : 'kollar',
-        'studentid' : 28283921,
-        'classid' : 1112332123,
-        'class' : 'Math Intro',
-        'classid2' : 'CS283',
-        'grades' : [100, 70, 40, 100, 4, 28],
-        'assignments' : [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+#dummyclass = [
+#    {
+#        'datestart' : datetime.date(2021, 1, 1),
+#        'dateend' : datetime.date(2021, 3, 4),
+#        'firstname' : 'steve',
+#        'lastname' : 'kollar',
+#        'studentid' : 28283921,
+#        'classid' : 1112332123,
+#        'class' : 'Math Intro',
+#        'classid2' : 'CS283',
+#        'grades' : [100, 70, 40, 100, 4, 28],
+#        'assignments' : [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+#
+#    },
+#    {
+#       'datestart' : datetime.date(2021, 1, 1),
+#       'dateend' : datetime.date(2021, 3, 4),
+#       'firstname' : 'steve',
+#       'lastname' : 'kollar',
+#       'studentid' : 28283921,
+#       'classid' : 1112332123,
+#       'class' : 'Math Intro',
+#       'classid2' : 'CS283',
 
-    },
-    {
-        'datestart' : datetime.date(2021, 1, 1),
-        'dateend' : datetime.date(2021, 3, 4),
-        'firstname' : 'steve',
-        'lastname' : 'kollar',
-        'studentid' : 28283921,
-        'classid' : 1112332123,
-        'class' : 'Math Intro',
-        'classid2' : 'CS283',
-
-    }
-]
+#   }
+#]
 
 completed_dummy = [
     {
@@ -79,6 +94,7 @@ def homepage(request):
 
 #@login_required    # Decorator that checks to see if a user is logged in before allowing them to access these views
 def student1_1(request):
+
     context = {
         'dummy_data': dummy_data    # uses dummy data specified above for student1_1 view until we can replace with DB data
     }
